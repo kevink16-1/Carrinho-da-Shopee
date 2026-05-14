0952f2ba4e64810b26f555efb314b44e7b7ee83c
kevink16-1 <kevinkauan.santos@aluno.educa.go.gov.br>
2026-05-14 19:35:48 -0300

    Refactor cart.js for improved logging and item handling
    Updated console log messages for better formatting and clarity. Improved item removal logic and error handling.

--- Carrinho da shopee/src/services/cart.js
+++ Carrinho da shopee/src/services/cart.js
@@ -1,20 +1,14 @@
-//quais açoes meu carrinho pode fazer
-
-//CASOS DE USO
-// ✅ -> adicionar item no carrinho
 async function addItem(userCart, item) {
   userCart.push(item);
 }
 
-// ✅ -> calcular o total do carrinho
 async function calculateTotal(userCart) {
   console.log("\nShopee Cart TOTAL IS:");
 
   const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
-  console.log(`🎁Total: ${result}`);
+  console.log(`Total: R$ ${result.toFixed(2)}`);
 }
 
-// -> deletar item do carrinho
 async function deleteItem(userCart, name) {
   const index = userCart.findIndex((item) => item.name === name);
 
@@ -23,38 +17,31 @@ async function deleteItem(userCart, name) {
   }
 }
 
-// -> ✅ remover um item - diminui um item
 async function removeItem(userCart, item) {
-  //1. encontrar o indice do item
   const indexFound = userCart.findIndex((p) => p.name === item.name);
 
-  //2. Caso não encontre o item
-  if (indexFound == -1) {
-    console.log("item não encontrado");
+  if (indexFound === -1) {
+    console.log("item nao encontrado");
     return;
   }
 
-  //3. item > 1 subtrair um item
   if (userCart[indexFound].quantity > 1) {
     userCart[indexFound].quantity -= 1;
     return;
   }
 
-  //4. caso item = 1 deletar o item
-  if (userCart[indexFound].quantity == 1) {
+  if (userCart[indexFound].quantity === 1) {
     userCart.splice(indexFound, 1);
-    return;
   }
 }
 
-// ✅ mostra todos os items do carrinho
 async function displaycart(userCart) {
   console.log("\nShopee cart list:");
   userCart.forEach((item, index) => {
     console.log(
-      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+      `${index + 1}. ${item.name} - R$ ${item.price.toFixed(2)} | ${
         item.quantity
-      }x | Subtotal = ${item.subtotal()}`
+      }x | Subtotal = R$ ${item.subtotal().toFixed(2)}`
     );
   });
 }
